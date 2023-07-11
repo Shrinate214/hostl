94e0c04d835b7018cf06bd5e28dfbd924f05dcc2
Akshat Shrinate <97226829+Shrinate214@users.noreply.github.com>
2023-07-11 18:38:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+var mysql=require("mysql2");
 const app = express();
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+const fs=require('fs')
+const multer = require('multer')
+const csv = require('fast-csv');
 app.use(express.static("public"));
 
 const port = 3000;
@@ -31,7 +36,26 @@ const UserSchema = {
     selectedValue: String
 };
 const User = new mongoose.model("User", UserSchema);
-
+var con = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password:"akshat123",
+    database: "hostel",
+    multipleStatements: true
+  });
+  // multer library configuration 
+  var storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, './uploads')    
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+ //??pass the stroage to multer library ka constructor means we store it locally for the time being
+let upload=multer({
+    storage:storage
+}); 
 //------------ Get ----------------------//
 
 app.get("/", function (req, res) {
@@ -112,3 +136,98 @@ app.post("/login", async (req, res) => {
 app.listen(port, function () {
     console.log(`Weather app listening on port ${port}`);
 });
+app.get("/upload",function(req,res){
+    res.render("upload");
+});
+app.post('/import-csv',upload.single("import-csv"),(req,res) =>{
+console.log(req.file.path);
+const Year_chosen= req.body.Positions;
+uploadcsv(__dirname+"/"+req.file.path,Year_chosen,res);
+
+var file_path=req.file.path;
+});
+
+
+//??
+
+
+function uploadcsv(path,Year_chosen,res){
+    let stream=fs.createReadStream(path);
+    let DataCollection=[];
+    let filestream=csv
+.parse()
+.on('data',function(data){
+    DataCollection.push(data) //passes the extracted data from the stored file in uploads folder to itself 
+})
+.on('end',function(){
+    DataCollection.shift(); //passes the extracted data from the stored file in uploads folder to itself 
+con.getConnection((error,connection) => {
+    if(error) {
+        console.error(error);
+        return;
+    }
+    else{ query=`
+    INSERT INTO students_to_be_alloc (timeStamps, name, email_id, SID, city_state, fees_paid, batch, branch)
+    VALUES ?;
+    INSERT INTO distinct_students_to_be_alloc (timeStamps, name, email_id, SID, city_state, fees_paid, batch, branch)
+    SELECT timeStamps, name, email_id, SID, city_state, fees_paid, batch, branch
+    FROM students_to_be_alloc
+    WHERE SID NOT IN (SELECT SID FROM distinct_students_to_be_alloc);
+    delete from students_to_be_alloc;
+    UPDATE distinct_students_to_be_alloc AS s
+    JOIN (
+       SELECT
+          s.SID,
+          h.room_number
+       FROM (
+          SELECT SID,
+             (ROW_NUMBER() OVER (ORDER BY RAND()) % (SELECT COUNT(*) FROM distinct_students_to_be_alloc WHERE fees_paid = 'yes')) + 1 AS rn
+          FROM distinct_students_to_be_alloc
+          WHERE fees_paid = 'yes' and batch =? and Room_assigned IS NULL
+       ) AS s
+       JOIN (
+          SELECT room_number,
+             ROW_NUMBER() OVER (ORDER BY RAND()) AS rn
+          FROM hostel_rooms
+       ) AS h
+       ON s.rn = h.rn
+    ) AS dt
+    ON s.SID = dt.sid
+    SET s.Room_assigned = dt.room_number; 
+    
+    UPDATE hostel_rooms AS h
+    JOIN distinct_students_to_be_alloc AS s ON s.room_assigned = h.room_number
+    SET h.room_status = 'unavailable';
+    
+ insert into Saved_results (name ,email_id ,SID ,batch , room_number,floor,block)    
+ SELECT distinct_students_to_be_alloc.name, distinct_students_to_be_alloc.email_id, distinct_students_to_be_alloc.SID, 
+    distinct_students_to_be_alloc.batch, hostel_rooms.room_number, hostel_rooms.floor, hostel_rooms.block
+    FROM distinct_students_to_be_alloc
+    JOIN hostel_rooms ON distinct_students_to_be_alloc.Room_assigned = hostel_rooms.room_number;
+select * from Saved_results order by room_number  ASC;
+    
+       `;
+
+  con.query(query,[DataCollection,Year_chosen], (error,results)=>
+        {
+    if(error){
+        console.log(error);
+             }
+    else      { console.log("Sexy Bancho");
+    const data = results[results.length - 1]; // Assuming the last result set contains the desired data
+
+    // Render the EJS template and pass the data as a variable
+    res.render('allocation_results', { data: data });
+             }
+
+        });
+      }
+    });
+ fs.unlinkSync(path); 
+  });
+
+stream.pipe(filestream); 
+} 
+
+
+
