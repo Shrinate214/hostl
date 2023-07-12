cc2e70cfe6200c1387aecb711fcf5260a897eb12
Akshat Shrinate <97226829+Shrinate214@users.noreply.github.com>
2023-07-12 12:55:45 +0530

    Add files via upload

---
+++ AKSHAT.sql
@@ -0,0 +1,6 @@
+update distinct_students_to_be_alloc
+set Room_assigned=null;
+select *from  students_to_be_alloc;
+select *from  distinct_students_to_be_alloc;
+delete from Saved_results;
+select * from Saved_results;
